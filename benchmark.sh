d37a113f450141d8d49b1d10c7cf6d9f0124d57f
bhavya <bhavya25hirani@gmail.com>
2026-04-07 14:20:37 -0500

    more changes to benchapp setup

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-BENCHMARKS=("stencil_chain" "interpolate" "max_filter" "camera_pipe" "hist" "nl_means" "lens_blur" "bilateral_grid" "harris" "local_laplacian" "conv_layer" "iir_blur" "bgu" "unsharp" "depthwise_separable_conv")  # add yours
-#BENCHMARKS=("lens_blur")  # add yours
+#BENCHMARKS=("stencil_chain" "interpolate" "max_filter" "camera_pipe" "hist" "nl_means" "lens_blur" "bilateral_grid" "harris" "local_laplacian" "conv_layer" "iir_blur" "bgu" "unsharp" "depthwise_separable_conv")  # add yours
+BENCHMARKS=("lens_blur" "interpolate" "camera_pipe" "local_laplacian" "iir_blur")  # add yours
 
 RESULT_DIR="./results_benchapp"
 #if [ -d "$RESULT_DIR" ]; then
@@ -14,8 +14,8 @@ RESULT_DIR="./results_benchapp"
 for BENCHMARK in "${BENCHMARKS[@]}"; do
     echo ">>> Running benchmark: $BENCHMARK"
 
-    #bash run_benchapp_samples.sh "$BENCHMARK" custom     | tee "$RESULT_DIR/${BENCHMARK}_custom.log"
-    #bash run_benchapp_samples.sh "$BENCHMARK" adams2019Libtorch  | tee "$RESULT_DIR/${BENCHMARK}_adams2019.log"
+	bash run_benchapp_samples.sh "$BENCHMARK" custom adams2019    | tee "$RESULT_DIR/${BENCHMARK}_custom.log"
+	#bash run_benchapp_samples.sh "$BENCHMARK" adams2019Libtorch  | tee "$RESULT_DIR/${BENCHMARK}_adams2019.log"
     bash run_benchapp_samples.sh "$BENCHMARK" adams2019Halide  | tee "$RESULT_DIR/${BENCHMARK}_adams2019Halide.log"
-    #bash run_benchapp_baseline.sh "$BENCHMARK" manual    | tee "$RESULT_DIR/${BENCHMARK}_manual.log"
+	bash run_benchapp_baseline.sh "$BENCHMARK" manual    | tee "$RESULT_DIR/${BENCHMARK}_manual.log"
 done
