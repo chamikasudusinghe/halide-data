062e8952c3c4fbf5f94ec1844e1d54d94e1af92d
bhavya <bhavya25hirani@gmail.com>
2026-08-03 15:48:56 -0500

    added build_generator script; path changes to makefile

---
+++ build_generator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+export HALIDE_ROOT=/home/bhavya/cosmos/life/UIUC/academics/research/halide/Halide
+BASELOC=`pwd`
+PIPELINE="random_pipeline"
+BLD_TOP=${BASELOC}/build_x86_generator_deeptuner
+
+
+#########################################
+# Step 1: Build random pipeline generator
+
+echo "###### Step 1: Building random_pipeline.generator and runtime for it..." 
+
+if [ ! -d ${BLD_TOP} ];
+then
+    BLD_TOP=${BLD_TOP} CXX=clang++ PIPELINE_SEED=0001 HALIDE_ROOT=${HALIDE_ROOT} make clean build
+echo "...Done. Continue to next step"
+else
+    echo "...Already built. Continue to next step."
+fi;
+
